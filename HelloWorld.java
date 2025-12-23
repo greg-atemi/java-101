2ab89aa3bb7a54fcfb2d9235bf49d6d84c31c990
gregatemi <magrega7180@gmail.com>
2025-12-23 14:55:14 +0300

    Initial commit

---
+++ HelloWorld.java
@@ -0,0 +1,7 @@
+public class HelloWorld {
+j    public static void main(String[] args) {
+        System.out.println("Hello, Java 11!");
+        System.out.println("Hello, Java 17!");
+        System.out.println("Hello, Java 21!");
+    }
+}
